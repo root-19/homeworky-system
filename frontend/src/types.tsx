8cb2478b6650b9d936fc413a33dbbb815ec7c9d0
rens20 <wasieacuna@gmail.com>
2024-08-02 00:23:21 +0800

    push

---
+++ frontend/src/types.tsx
@@ -0,0 +1,17 @@
+export type Assignment = {
+  title: string;
+  description: string;
+  due_date: string;
+  completed: boolean;
+};
+
+// export interface User {
+//   id: number;
+//   name: string;
+//   // other user fields
+// }
+
+// export interface ChatProps {
+//   user: User | null; // Allowing user to be null initially
+// }
+export type Assignments = Assignment[];
